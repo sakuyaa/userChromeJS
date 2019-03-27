580538e8c5c6c0d1c5d61957167bfd061414be78
sakuyaa <sakuyaa@users.noreply.github.com>
2019-03-27 14:54:34 +0800

    修复火狐66标签栏顶上多余的空间

--- AppButton.uc.js
+++ AppButton.uc.js
@@ -3,16 +3,16 @@
 // @author		sakuyaa
 // @description	左上角火狐按钮
 // @include		main
-// @version		2015.8.12
+// @version		2019.3.20
 // @charset		UTF-8
 // @homepageURL	https://github.com/sakuyaa/userChromeJS
 // ==/UserScript==
 (function() {
-	var appButton = document.getElementById('PanelUI-button');
-	document.getElementById('TabsToolbar').insertBefore(appButton, document.getElementById('tabbrowser-tabs'));
+	let appButton = document.getElementById('PanelUI-button');
+	document.getElementById('TabsToolbar').insertBefore(appButton, document.getElementById('TabsToolbar').firstChild);
 	appButton.style.border = 'none';
-	var menu = document.getElementById('PanelUI-menu-button');
-	menu.style.padding = '2px 8px';
+	let menu = document.getElementById('PanelUI-menu-button');
+	menu.style.padding = '0 3px';
 	menu.style.marginLeft = '-3px';
 	menu.style.MozImageRegion = 'rect(0, 0, 0, 0)';
 	menu.style.listStyleImage = 'url("chrome://branding/content/icon32.png")';   //火狐图标
