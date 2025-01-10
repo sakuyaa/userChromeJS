cc2fffc121c63008d34689e43167b46e79f4845d
sakuyaa <sakuyaa@users.noreply.github.com>
2025-01-11 00:01:29 +0800

    增加延迟避免运行太早获取不到gBrowser

--- MouseScrollCloseTab.uc.js
+++ MouseScrollCloseTab.uc.js
@@ -3,22 +3,28 @@
 // @namespace	zhaibr@163.com
 // @description	鼠标悬停标签滚轮上下关闭该标签
 // @include		main
-// @version		2016.8.5
+// @version		2025.1.10
 // @charset		UTF-8
 // @homepageURL	https://github.com/sakuyaa/userChromeJS
 // @note		modify by sakuyaa
 // ==/UserScript==
 (function() {
-	var time = true;
-	gBrowser.mTabContainer.addEventListener('wheel', event => {
-		if (event.target.localName == 'tab' && !event.ctrlKey && time) {
-			time = false;
-			gBrowser.removeTab(event.target);
-			event.stopPropagation();
-			event.preventDefault();
-			setTimeout(() => {   //关闭延迟
-				time = true;
-			}, 300);
-		}
-	}, true);
-}());
+	let enableClose = true;
+	setTimeout(() => {   //增加延迟避免运行太早获取不到gBrowser
+		gBrowser.tabContainer.addEventListener('wheel', event => {
+			if (!event.ctrlKey && enableClose) {
+				let tab = event.target.closest('.tabbrowser-tab');
+				if (!tab) {
+					return;
+				}
+				enableClose = false;
+				gBrowser.removeTab(tab, {animate: true});
+				event.stopPropagation();
+				event.preventDefault();
+				setTimeout(() => {
+					enableClose = true;
+				}, 500);   //短时间内不能连续关闭
+			}
+		}, true);
+	}, 831);
+})();
